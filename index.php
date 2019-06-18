d9952b5750ffba8f9c4d06b1e6914f70b91d52f1
David Peng <dpeng1@gmail.com>
2019-06-18 07:45:19 -0700

    Removed urchin

--- index.php
+++ index.php
@@ -17,12 +17,7 @@ body {
 }
 -->
 </style>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-2919861-11";
-urchinTracker();
-</script>
+
 
 </style>
 <link href="merideon.css" rel="stylesheet" type="text/css">
